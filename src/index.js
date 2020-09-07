4d416a92f4593ed176eab1158f5b48b302df3921
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-09-07 19:31:31 +0100

    chore: revert babel-minify implementation, pending methodology for web3 conditional exports

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const fse = require("fs-extra");
 const npm = require("npm-programmatic");
 const browserify = require("browserify");
 const camel = require("camelcase");
+const minify = require("babel-minify");
 
 const createStub = ({pkg, pkgName, polyfills}) => `
 ${polyfills.map(polyfill => `require("${polyfill}");`)}
@@ -15,7 +16,8 @@ module.exports = ${pkgName};
 `.trim();
 
 const restructure = async (outFile, {pkg, pkgName}) => {
-  const code = `
+  const { code } = minify(
+    `
 var ${pkgName};
 
 ${(await fs.readFileSync(outFile, "utf8"))
@@ -23,7 +25,12 @@ ${(await fs.readFileSync(outFile, "utf8"))
   .replace(`module.exports = ${pkgName};`, "")}
 
 module.exports = ${pkgName};
-  `.trim();
+    `.trim(),
+    {
+      mangle: false,
+      regexpConstructors: false,
+    },
+  );
 
   await fs.writeFileSync(outFile, code);
 };
